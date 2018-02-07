554e84e3fc6861275b7f9bf6bcdcbde69bcfd42b
Ryder Dain <ryder.dain@gmail.com>
2018-02-07 16:12:28 +0100

    Compat improvements, adding file-read option

--- checkssl.sh
+++ checkssl.sh
@@ -2,21 +2,40 @@
 
 
 ## Pretty Colors!
+#   NOTE: 'E' def left at end for when `cat` used on terminal
 R='[31m'; G='[32m'; Y='[33m'; B='[34m'; M='[35m'; C='[36m';
 Bold='[1m';
 Underline='[4m';
 E='[0m';
 
+
+## Usage
+DESCRIPTION="
+Pretty-prints useful info about the SSL certificate. Can fetch via DNS
+entry or checks a file.
+"
+USAGE="
+Usage:
+    $0 <fqdn|file>
+
+"
+
 ## Compatibility
-if [ "`uname -o`" = "FreeBSD" ]
+if [ "`uname`" = "FreeBSD" ]
 then
     CA_ROOTS=/etc/ssl/cert.pem
     STR2DATE="date -jf "
-elif [ "`uname -o`" = "Linux" ]
+elif [ "`uname`" = "Darwin" ]
+then
+    ## WARNING: older Bourne Shell is nonexistent on Darwin
+    CA_ROOTS=/etc/ssl/cert.pem
+    STR2DATE="date -jf "
+elif [ "`uname`" = "Linux" ]
 then
     CA_ROOTS=/etc/ssl/cert.pem
     STR2DATE="date -d "
 fi
+
 
 ## Utilities
 getip() {
@@ -29,7 +48,8 @@ getsuf() {
 }
 error() {
     ret_code=${1}; shift
-    echo "`date` [error]: ${@}"
+    printf "%s [error]: %s" "`date`" "${@}"
+    printf "%s" "$USAGE"
     exit ${ret_code}
 }
 
@@ -51,18 +71,18 @@ print_expiry() {
     sslEndDate="`openssl x509 -noout -enddate -in "${CERT}" | sed -e 's/\(.*\)=\(.*\)/\2/'`"
     if ! (check_expiry ${CERT} 0)
     then 
-        echo "EXPIRED: ${Bold}${R}$sslEndDate${E}"
+        printf "EXPIRED: ${Bold}${R}$sslEndDate${E}"
     elif ! (check_expiry ${CERT} 30)
     then
-        echo "Expires Soon: ${Bold}${Y}$sslEndDate${E}"
+        printf "Expires Soon: ${Bold}${Y}$sslEndDate${E}\n"
     else
-        echo "Expires: ${G}$sslEndDate${E}"
+        printf "Expires: ${G}$sslEndDate${E}\n"
     fi
 }
 print_altnames() {
     ALTNAMES="`openssl x509 -noout -text -in \"${1}\" | grep -oE 'DNS:.*[^,$ ]' | sed -e 's/,//g;s/DNS:\([^ $]*\)/\1 /g'`"
     count=0
-    echo "___ Subject Alternative Names ___"
+    printf "___ Subject Alternative Names ___\n"
     for an in $ALTNAMES
     do
         printf "%02d: %s\n" $count $an
@@ -72,7 +92,7 @@ print_altnames() {
 print_subject() {
     cyan="$C"
     eval "`openssl x509 -noout -subject -in \"${1}\" | sed -e 's/[^/]*\/\([^=]*\)=\([^/]*\)/\1=\"\2\"; /g'`"
-    echo "
+    printf "
 ___ Subject ___
 Country: $C
 Location: $L
@@ -88,45 +108,65 @@ print_start() {
     sslStartDate="`openssl x509 -noout -startdate -in "${CERT}" | sed -e 's/\(.*\)=\(.*\)/\2/'`"
     if (check_start ${CERT} 0)
     then 
-        echo "Ready Since: ${G}$sslStartDate${E}"
+        printf "Ready Since: ${G}$sslStartDate${E}\n"
     elif (check_start ${CERT} 7)
     then
-        echo "Ready Soon: ${Y}$sslStartDate${E}"
+        printf "Ready Soon: ${Y}$sslStartDate${E}\n"
     else
-        echo "NOT READY: ${Bold}${R}$sslStartDate${E}"
+        printf "NOT READY: ${Bold}${R}$sslStartDate${E}\n"
     fi
 }
 print_general() {
     CERT=${1}
-    echo "${Bold}${G}___ ALL INFO ON ${C}${FQDN}${G} ___${E}"
+    printf "\n${Bold}${G}___ CORE INFO ON ${C}${FQDN}${G} ___${E}\n"
     openssl x509 -noout -subject -issuer -dates -in "${CERT}" | sed -e "s/\(.*\)=\(.*\)/\1=${Bold}${Y}\2${E}/"
 }
 
 ## Main!
 main() {
-    FQDN="${1}"; shift
-    FQDN_IP=`getip ${FQDN}`
-    if [ -z "$FQDN_IP" ]
+    if [ -z "${1}" ]; then printf "%s%s" "$DESCRIPTION" "$USAGE"; exit 0; fi
+
+    if [ -f "${1}" ] && (grep -q "BEGIN CERTIFICATE" "$1")
+
+    ## First Option: Check by File
     then
-        error 1 "Found no IP for \"$FQDN\""
+        CERT_CHAIN="${1}" ; shift
+        printf "${G}EXTRACTING CN FROM ${C}${CERT_CHAIN}${G} ...${E} "
+        eval "`openssl x509 -noout -subject -in \"${CERT_CHAIN}\" | sed -e 's/.*CN *=\([^/]*\)/CN="\1"; /g'`"
+        FQDN="$CN"
+        FQDN_IP=`getip ${FQDN}`
+        if [ $? -gt 0 ]
+        then
+            printf "${R}NO IP FOR CN \"${C}${Bold}${FQDN}${E}\"\n" 
+        else
+            printf "\"${C}${Bold}${FQDN}${E}\" ${G}LIVES AT ${C}${FQDN_IP}${E}\n" 
+        fi
+
+    ## Second Option: Check by FQDN
+    else
+        FQDN="${1}"; shift
+        FQDN_IP=`getip ${FQDN}`
+        if [ -z "$FQDN_IP" ]
+        then
+            error 1 "Found no IP for \"$FQDN\""
+        fi
+        while
+            CERT_CHAIN=/tmp/${FQDN}_sslchain-`getsuf`.txt
+            [ -f "$CERT_CHAIN" ]
+        do continue ; done
+        touch $CERT_CHAIN
+
+        printf "${G}CONNECTING TO ${C}${FQDN_IP}${G} TO RETRIEVE CERT FOR \"${Bold}${C}${FQDN}${G}\"...${E}\n"
+        printf "\n" | openssl s_client -showcerts -servername "$FQDN" -connect $FQDN_IP:443 2>/dev/null > $CERT_CHAIN
     fi
 
-    while
-        CERT_CHAIN=/tmp/${FQDN}_sslchain-`getsuf`.txt
-        [ -f "$CERT_CHAIN" ]
-    do continue ; done
-    touch $CERT_CHAIN
-
-    echo "${G}CONNECTING TO ${C}${FQDN_IP}${G} TO RETRIEVE CERT FOR \"${Bold}${C}${FQDN}${G}\"...${E}"
-    echo "" | openssl s_client -showcerts -servername "$FQDN" -connect $FQDN_IP:443 2>/dev/null > $CERT_CHAIN
-
-    echo -n "${G}VERIFYING ${Bold}${C}${FQDN}${E} ${G}AGAINST $CA_ROOTS via "
+    printf "${G}VERIFYING ${Bold}${C}${FQDN}${E} ${G}AGAINST $CA_ROOTS via "
     verify_out="`openssl verify -verbose -CAfile $CA_ROOTS -untrusted $CERT_CHAIN $CERT_CHAIN`"
     if [ $? -gt 0 ]
     then
-        echo "${R}$verify_out${E}"
+        printf "${R}$verify_out${E}\n"
     else
-        echo "${Bold}${G}$verify_out${E}"
+        printf "${Bold}${G}$verify_out${E}\n"
     fi
 
     print_general ${CERT_CHAIN}
@@ -135,7 +175,6 @@ main() {
     print_start ${CERT_CHAIN}
     print_expiry ${CERT_CHAIN}
 
-    rm $CERT_CHAIN
     exit 0
 }
 
